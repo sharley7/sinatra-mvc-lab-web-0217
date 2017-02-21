bda4a98fed363baf5f2dcb37dbecb6bb38d938fa
Siobhan <siobhan.harley@gmail.com>
2017-02-20 23:54:45 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+
+    get '/' do
+    erb :index
+    end
+
+
+
+    post '/piglatinize' do
+      pig = PigLatinizer.new
+      @pig_text = pig.to_pig_latin(params[:user_phrase])
+      erb :piglatinize
+    end
+
+
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,30 @@
+require 'pry'
+class PigLatinizer
+
+def piglatinize(word)
+   non_pig_latin_words = ["I", "me", "to", "too", "a", "an", "in", "and", "on"]
+   vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+
+   if non_pig_latin_words.include?(word)
+     word + 'way'
+   elsif vowels.include? word[0]
+     word + "way"
+   else
+     consonants = ""
+     while !vowels.include?(word[0])
+       consonants << word[0]
+       word = word.split("")[1..-1].join
+     end
+     word + consonants + 'ay'
+
+   end
+ end
+
+
+  def to_pig_latin(string)
+    string.split.collect{|word| piglatinize(word)}.join(" ")
+  end
+
+
+
+end
